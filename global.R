7f2e4d4fa6400c9706597322adfea7e6c1abb0a0
nrukt00vt <nrukt00@vt.edu>
2023-11-10 18:38:10 -0500

    beginning to add month selector

--- global.R
+++ global.R
@@ -6,15 +6,15 @@ library(tidyverse)
 shapefile = read_sf(dsn ="base_files", layer= "tl_2022_51_bg")
 #Choose HealthPOIs_Montgomery_VA.csv
 health_POIs = read.csv('HealthPOIs_Montgomery_VA 2.csv')
-montgomery_health_POIs = subset(health_POIs, city == "Blacksburg" | city == "Christiansburg")%>% distinct(street_address, .keep_all = TRUE)
 
 
 #Read in data
-all_data = read.csv("overall_trips_VA.csv")
-overall_trips = merge(montgomery_health_POIs,all_data, by.x="safegraph_place_id",by.y="safegraph_place")
+all_data = read.csv("NRV_monthly_data.csv")[,-1]
+all_data = subset(all_data,!is.na(all_data$visitor_home_cbg) & num > 0)
+overall_trips = merge(health_POIs,all_data, by.x="safegraph_place_id",by.y="safegraph_place")
 
 uniqueLocations <- overall_trips %>% distinct(safegraph_place_id, .keep_all = TRUE)
 uniqueNAICS = unique(uniqueLocations$naics_code)
-maximum_value = max(aggregate(overall_trips[,c("total_visitors")],by = list(overall_trips$visitor_home_cbg, overall_trips$naics_code),FUN = sum)$x)
+maximum_value = max(aggregate(overall_trips[,c("num")],by = list(overall_trips$visitor_home_cbg, overall_trips$naics_code, overall_trips$month),FUN = sum)$x)
 print("done prep")
 
--- ui.R
+++ ui.R
@@ -11,6 +11,13 @@ sidebarLayout(
                 selected = 0),
     selectInput("POI_selection", "Select Individual POI: ",
                 choices = NULL),
+    sliderInput("Month_selection",
+                "Dates:",
+                min = min(as.Date(overall_trips$month)),
+                max = max(as.Date(overall_trips$month)),
+                value=as.Date("2019-12-01"),
+                timeFormat="%Y-%m"),
+    
     tableOutput("UI_table"),
   ),
 
