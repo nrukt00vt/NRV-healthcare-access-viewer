937aeefcb2569cdf7e2f796a90f8958153d9114e
Nick Ruktanonchai <nrukt00@gmail.com>
2023-11-17 22:15:04 -0500

    Update distance_calculation.R

--- distance_calculation.R
+++ distance_calculation.R
@@ -7,11 +7,12 @@ shapefile = read_sf(dsn ="base_files", layer= "tl_2019_51_bg")
 shapefile = subset(shapefile,is.element(COUNTYFP,c("063","121","155","750","071")))
 
 #We won't worry about isolating to Blacksburg and Christiansburg
-montgomery_health_POIs = read.csv('HealthPOIs_Montgomery_VA 2.csv')
+health_POIs = read.csv('HealthPOIs_Montgomery_VA 2.csv')
 
 
 #Read in data
 all_data = read.csv("NRV_monthly_data.csv")
+names(all_data)[which(names(all_data) == "num")] = "total_visitors"
 overall_trips = merge(health_POIs,all_data, by.x="safegraph_place_id",by.y="safegraph_place")
 
 #We will only use the CBGs that actually appear in the dataset, just to save processing time
@@ -22,28 +23,21 @@ centroids_cbgs = st_centroid(shapefile) %>% st_set_crs(4326)
 
 #Get all unique healthcare IDs; we will start to fill in a dataframe where we also fill in the average travel distance to there
 unique_healthcare_ids = unique(overall_trips$safegraph_place_id)
-
-healthcare_dist = data.frame(ID = unique_healthcare_ids, distance = 0)
-
+unique_months = unique(overall_trips$month)
+healthcare_dist = expand.grid(ID = unique_healthcare_ids, distance = 0,month = unique_months)
 
 #### Healthcare facility based distance calculation
 #below this, I have written code to run the first healthcare facility; you'll want to edit and create a for loop here
-
 #healthcare_dist$ID tells R to look at the ID variable inside of healthcare_dist
-for (i in 1:length(healthcare_dist$ID)){
-  
-  working_id = healthcare_dist$ID[i]
-  print(working_id)
-  
-}
+
 
 for (i in 1:length(healthcare_dist$ID)){
-  
+  working_month = healthcare_dist$month[i]
   working_id = healthcare_dist$ID[i]
   print(working_id)
   
   #subset all trips to the healthcare facility
-  trips_to_healthcare = subset(overall_trips,safegraph_place_id == working_id)
+  trips_to_healthcare = subset(overall_trips,safegraph_place_id == working_id & month == working_month)
   #associate each row with the corresponding CBG centroid
   trips_to_healthcare_w_centroids = merge(trips_to_healthcare,centroids_cbgs,by.x="visitor_home_cbg",by.y="GEOID")
   trips_to_healthcare_w_centroids = st_as_sf(trips_to_healthcare_w_centroids) %>%   st_set_crs(4326)
@@ -64,6 +58,8 @@ for (i in 1:length(healthcare_dist$ID)){
   
 }
 
+
+
 #plotting
 healthcare_dist = subset(healthcare_dist, distance > 0)
 
@@ -82,21 +78,17 @@ ggplot() + geom_sf(data = health_POIs_sf_dist, mapping = aes(colour = distance))
 #This will use broadly the same logic, just will be focused on CBGs rather than healthcare facilities
 unique_cbg_ids = unique(overall_trips$visitor_home_cbg)
 
-cbg_dist = data.frame(ID = unique_cbg_ids, distance = 0)
+cbg_dist = expand.grid(ID = unique_cbg_ids, distance = 0,month = unique_months)
 
 # start with the first one
 working_id = cbg_dist$ID[1]
 
 for (i in 1:length(cbg_dist$ID)){
   working_id = cbg_dist$ID[i]
-  print(working_id)
-}
-
-for (i in 1:length(cbg_dist$ID)){
-  working_id = cbg_dist$ID[i]
+  working_month = cbg_dist$month[i]
   print(working_id) 
 
-  trips_fr_cbg = subset(overall_trips,visitor_home_cbg == working_id)
+  trips_fr_cbg = subset(overall_trips,visitor_home_cbg == working_id& month == working_month)
   
   trips_fr_cbg =  st_as_sf(trips_fr_cbg, coords = c("longitude","latitude"))%>% 
     st_set_crs(4326)
