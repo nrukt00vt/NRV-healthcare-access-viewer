8a5653daf52eccc90f7facf8257f5c0134f09012
nrukt00vt <nrukt00@vt.edu>
2024-01-29 13:45:03 -0500

    subset to NRV

--- global.R
+++ global.R
@@ -6,9 +6,10 @@ library(lubridate)
 #Read in the shapefile as an "sf" object
 shapefile = read_sf(dsn ="base_files", layer= "tl_2019_51_bg")
 #Choose HealthPOIs_Montgomery_VA.csv
+shapefile = subset(shapefile,is.element(COUNTYFP,c("063","121","155","750","071")))
 health_POIs = read.csv('HealthPOIs_Montgomery_VA 2.csv')
 
-
+health_POIs= subset(health_POIs, postal_code != 20176)
 #Read in data
 all_data = read.csv("NRV_monthly_data.csv")[,-1]
 all_data = subset(all_data,!is.na(all_data$visitor_home_cbg) & num > 0)
