c4347ffdefcb6ff64e7b8d6652c8993b77efd34d
SyedaNahidFatema <syedanahidfatema920@gmail.com>
2024-12-08 01:58:15 -0500

    trips_distances

--- trips_distances.R
+++ trips_distances.R
@@ -7,16 +7,22 @@ library(tmaptools)
 remotes::install_github("riatelab/osrm")
 
 
+
+route = osrmRoute(src = #this should be the lat and then lon of the cbg
+                    #in poi_trips_sub, this will look something like poi_trips_sub$cbg_lat[1] and then poi_trips_Sub$cbg_lon[1]
+                    c(cbgs[1], cbgs[2]) ,  
+                  #poi_trips_sub$poi_lat[1] poi_trips_sub$poi_lon[1]
+                  dst = c(pois[1], pois[2]), osrm.profile = "car")
+
+
 cbgs <- c(POI_trips_sub$cbg_lon[1], POI_trips_sub$cbg_lat[1])  
 pois <- c(POI_trips_sub$POI_lon[1], POI_trips_sub$POI_lat[1]) 
-
 
 print(cbgs) 
 print(pois)
 
 
 route <- osrmRoute(src = cbgs, dst = pois, osrm.profile = "car")
-
 
 print(route$distance)
 
@@ -41,25 +47,3 @@ View(distances)
 
 
 
-
-cbgs = as.numeric(cbg$coords)
-pois = as.numeric(pois$coords)
-
-cbgs[1] = POI_trips_sub$cbg_lat[1]
-cbgs[2] = POI_trips_sub$cbg_lon[2]
-pois[1] = POI_trips_sub$POI_lat[1]
-pois[2] = POI_trips_sub$POI_lon[2]
-
-
-route = osrmRoute(src = #this should be the lat and then lon of the cbg
-#in poi_trips_sub, this will look something like poi_trips_sub$cbg_lat[1] and then poi_trips_Sub$cbg_lon[1]
-                    c(cbgs[1], cbgs[2]) ,  
-                  #poi_trips_sub$poi_lat[1] poi_trips_sub$poi_lon[1]
-                  dst = c(pois[1], pois[2]), osrm.profile = "car")
-
-
-route = osrmRoute(src = c(cbgs[1], cbgs[2]) , dst = c(pois[1], pois[2]) , osrm.profile = "car")
-
-
-route$distance
-view(distances)
